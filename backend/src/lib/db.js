21a6d0a424ab3f806d6a77147b4cf56091308eb3
zafor-hridoy <jaforhridoy003@gmail.com>
2026-01-08 04:45:32 +0600

    all resolved

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose"
 import { ENV } from "./env.js"
 
-export const connectDB = async () => 
-{
-    try
-    {
-        const con = await mongoose.connect(process.env.MONGO_URI)
+export const connectDB = async () => {
+    try {
+        const { MONGO_URI } = ENV;
+        if (!MONGO_URI) throw new Error("isn't step up");
+        const con = await mongoose.connect(ENV.MONGO_URI)
         console.log("mongobd connected: ", con.connection.host)
     }
-    catch(error)
-    {
-            console.error("error: ", error)
-            process.exit(1);
+    catch (error) {
+        console.error("error: ", error)
+        process.exit(1);
     }
 }
