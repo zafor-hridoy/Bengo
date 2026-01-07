d507b6bd5767ac71749ed9b5af6a7c31eb9435c3
zafor-hridoy <jaforhridoy003@gmail.com>
2026-01-08 03:27:10 +0600

    signup error solved

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,7 +2,11 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
+<<<<<<< HEAD
   const JWT_SECRET = ENV.JWT_SECRET;
+=======
+  const { JWT_SECRET } = ENV;
+>>>>>>> 856d621 (signup error solved)
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not configured");
   }
@@ -15,7 +19,11 @@ export const generateToken = (userId, res) => {
     maxAge: 604800000,
     httpOnly: true,
     sameSite: "strict",
+<<<<<<< HEAD
     secure: ENV.NODE_ENV !== "development",
+=======
+    secure: ENV.NODE_ENV === "development" ? false : true,
+>>>>>>> 856d621 (signup error solved)
   });
 
   return token;
