7a969ebda02e39fefd889a7ff6a75d9de5feb636
zafor-hridoy <jaforhridoy003@gmail.com>
2026-01-08 02:07:31 +0600

    fourth maybe

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+
 export const signup = async(req,res) =>
 {
     const {fullName, email, password} = req.body
@@ -15,9 +18,38 @@ export const signup = async(req,res) =>
         if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Invalid email format" });
     }
+        const user = await User.findOne({email})
+        if(user) 
+            return res.status(400).json({message:"Email already exists"})
+      
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword =  await bcrypt.hash(password,salt)
+
+        const newUser = new User ({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+        if(newUser)
+        {
+            generateToken(newUser._id, res)
+            await newUser.save()
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic: newUser.profilePic,
+            })
+        }
+        else{
+            res.status(400).json({message: "Invalid User"})
+        }
     }catch (error)
     {
-
+        console.log("error: ", error)
+        res.status(500).json({message: "uneven error"})
     }
     
 }
