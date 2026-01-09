286aafa15ad753b7257bfc23a45cc96bb8274ad6
meghla-zaman <shanzidazaman113@gmail.com>
2026-01-09 19:52:58 +0600

    message get route

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -74,3 +74,29 @@ export const sendMessage = async (req, res) => {
   }
 };
 
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    });
+
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ];
+
+    const chatPartners = await User.find({ _id: { $in: chatPartnerIds } }).select("-password");
+
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.error("Error in getChatPartners: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
