9ef309e6db65b4ab5122fe7b75763268734f7485
zafor-hridoy <jaforhridoy003@gmail.com>
2026-01-10 03:05:09 +0600

    side bar half

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ContactList() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { getAllContacts, allContacts, setSelectedUser, isUsersLoading } = useChatStore();
+  const { onlineUsers } = useAuthStore();
 
-export default ContactList
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+  if (isUsersLoading) return <UsersLoadingSkeleton />;
+
+  return (
+    <>
+      {allContacts.map((contact) => (
+        <div
+          key={contact._id}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            <div className={'avatar online'}>
+              <div className="size-12 rounded-full">
+                <img src={contact.profilePic || "/avatar.png"} />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium">{contact.fullName}</h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+export default ContactList;
