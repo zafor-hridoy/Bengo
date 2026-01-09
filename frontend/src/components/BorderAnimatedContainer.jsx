12845177b5d1d20e7831ca346a0b33e1b6650082
meghla-zaman <shanzidazaman113@gmail.com>
2026-01-09 22:44:28 +0600

    signup page completed

---
+++ frontend/src/components/BorderAnimatedContainer.jsx
@@ -0,0 +1,8 @@
+function BorderAnimatedContainer({ children }) {
+  return (
+    <div className="w-full h-full [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border  flex overflow-hidden">
+      {children}
+    </div>
+  );
+}
+export default BorderAnimatedContainer;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        border: "border 4s linear infinite",
+      },
+      keyframes: {
+        border: {
+          to: { "--border-angle": "360deg" },
+        },
+      },
+    },
   },
   plugins: [
     require('daisyui'),
