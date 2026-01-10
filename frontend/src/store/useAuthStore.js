02f30e33d78493bcf3627625c98c4ef96462494d
zafor-hridoy <jaforhridoy003@gmail.com>
2026-01-11 02:23:59 +0600

    deploy 04

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from 'react-hot-toast';
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3001" : "https://bengo-backend-xxx.vercel.app";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3001" : "https://bengo-zdu5.vercel.app";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
